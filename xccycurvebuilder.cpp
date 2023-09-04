dfc375201ce07026e36fd197b0976c4391a5877d
FineFinance <42334069+FinancialEngineerLab@users.noreply.github.com>
2023-09-04 15:30:41 +0900

    Update and rename xccycurve.cpp to xccycurvebuilder.cpp

--- xccycurvebuilder.cpp
+++ xccycurvebuilder.cpp
@@ -1,29 +1,29 @@
-#include "xccycurve.hpp"
+#include "xccycurvebuilder.hpp"
 
 using namespace QuantLib;
 
 ext::shared_ptr<RateHelper> xccyCurveBuilder::getCcyRateHelper(
-                                             const xccyDatum& xccyData,
-                                             RelinkableHandle<YieldTermStructure> baseCcyIdxHandle,
-                                             RelinkableHandle<YieldTermStructure> quoteCcyIdxHandle,
-                                             ext::shared_ptr<IborIndex> baseCcyIdx,
-                                             ext::shared_ptr<IborIndex> quoteCcyIdx,
-                                             bool isFxBaseCurrencyCollateralCurrency,
-                                             bool isBasisOnFxBaseCurrencyLeg
-                                                                         )
+    const xccyDatum& xccyData,
+    RelinkableHandle<YieldTermStructure> baseCcyIdxHandle,
+    RelinkableHandle<YieldTermStructure> quoteCcyIdxHandle,
+    ext::shared_ptr<IborIndex> baseCcyIdx,
+    ext::shared_ptr<IborIndex> quoteCcyIdx,
+    bool isFxBaseCurrencyCollateralCurrency,
+    bool isBasisOnFxBaseCurrencyLeg
+)
 {
-    Handle<Quote> quoteHandle(ext::make_shared<SimpleQuote>( xccyData.basis * 1.0e-4));
+    Handle<Quote> quoteHandle(ext::make_shared<SimpleQuote>(xccyData.basis * 1.0e-4));
     Period tenor(xccyData.n, xccyData.units);
-    
+
     Handle<YieldTermStructure> collateralHandle =
-            isFxBaseCurrencyCollateralCurrency ? baseCcyIdxHandle : quoteCcyIdxHandle;
-    
+        isFxBaseCurrencyCollateralCurrency ? baseCcyIdxHandle : quoteCcyIdxHandle;
+
     ext::shared_ptr<RateHelper> ccyRateHelper(new ConstNotionalCrossCurrencyBasisSwapRateHelper(quoteHandle, tenor, m_settlementDays, m_calendar, m_businessConvention, true,
         baseCcyIdx, quoteCcyIdx,
         collateralHandle,
         isFxBaseCurrencyCollateralCurrency,
         isBasisOnFxBaseCurrencyLeg));
-    
+
     return ccyRateHelper;
 }
 
@@ -40,20 +40,20 @@ RelinkableHandle <YieldTermStructure> xccyCurveBuilder::xccyCurve(std::vector<xc
 
     // rate helper building ! //
     std::vector<ext::shared_ptr<RateHelper> > instruments;
-    
+
     instruments.reserve(xccyData.size());
     for (const auto& i : xccyData)
     {
         instruments.push_back(getCcyRateHelper(i, baseCcyIdxHandle, quoteCcyIdxHandle,
-                                                           baseCcyIdx,
-                                                           quoteCcyIdx,
-                                                           isFxBaseCurrencyCollateralCurrency,
-                                                           isBasisOnFxBaseCurrencyLeg));
+            baseCcyIdx,
+            quoteCcyIdx,
+            isFxBaseCurrencyCollateralCurrency,
+            isBasisOnFxBaseCurrencyLeg));
     }
     ext::shared_ptr<YieldTermStructure> foreignCcyCurve(
-            new PiecewiseYieldCurve<Discount, LogLinear>(settlement, instruments, m_dayCount));
+        new PiecewiseYieldCurve<Discount, LogLinear>(settlement, instruments, m_dayCount));
     foreignCcyCurve->enableExtrapolation();
     RelinkableHandle<YieldTermStructure> foreignCurve(foreignCcyCurve);
-    
+
     return foreignCurve;
 }
